11cfd6d6e439224e003473713e63820f296e8e96
Juliano Pasa <jdpasa@inf.ufsm.br>
2022-06-03 15:40:14 -0300

    Example

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MazeAbstraction.Tools.GraphTools;
+using MazeAbstraction.Tools.ImageTools;
+using System.Drawing;
+
+
+namespace MazeAbstraction.Source
+{
+    public class main
+    {
+        static void Main(){
+            Bitmap bmp = ImageLoader.LoadImage("D:/Juliano Pasa/Pesquisa/MazeAbstraction/Images/maze.png");
+            BinaryImage bimg = new BinaryImage(bmp, bmp.Width, bmp.Height);
+
+            AbstractGraph graph = BinaryImageTranslator.BinaryImageToAbstractGraph(bimg, 6);
+
+            ExtendedLink link = graph.GetLink(1, 1); 
+            List<MazeAbstraction.Tools.GraphTools.Point> points = link.GetintermediatePath();
+            foreach (MazeAbstraction.Tools.GraphTools.Point point in points){
+                Console.WriteLine("row " + point.row + " col " + point.col);
+            }
+        }
+    }
+}
